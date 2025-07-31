59b3ec7fbf168594e5a94969a2e49ba47d93271a
rohit003kumar <151962481+rohit003kumar@users.noreply.github.com>
2025-07-31 21:34:47 +0530

    Update Navbar.tsx

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -374,7 +374,9 @@
 
 import React, { useState, useEffect } from 'react';
 import InstallPWAButton from './InstallPWAButton';
-import { Link, useNavigate } from 'react-router-dom';
+// import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+
 import {
   Search,
   WashingMachine,
