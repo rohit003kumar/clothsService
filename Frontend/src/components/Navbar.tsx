ff190b4044f7c52e24d7d13a9b999f55a11cbe57
rohit003kumar <151962481+rohit003kumar@users.noreply.github.com>
2025-08-07 16:06:21 +0530

    Update Navbar.tsx

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -212,7 +212,15 @@ const handleLogout = () => {
               </li>
 
               <li>
-                <InstallPWAButton />
+{/*                 <InstallPWAButton /> */}
+<a
+  href="/my-laundry-app.apk"
+  download
+  className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
+>
+  📲 Download Android App
+</a>
+
               </li>
             </ul>
           </div>
