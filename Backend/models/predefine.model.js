8809a80bb4532c2a8e95c79beab8bd8bfba1785b
rohit003kumar <151962481+rohit003kumar@users.noreply.github.com>
2025-08-09 21:53:24 +0530

    Update predefine.model.js

--- Backend/models/predefine.model.js
+++ Backend/models/predefine.model.js
@@ -1,20 +1,5 @@
 
 
-// const mongoose = require("mongoose");
-
-// const slotTemplateSchema = new mongoose.Schema({
-//   date: { type: String, required: true }, // "2025-07-01"
-//   slots: [{
-//     label: String,              // e.g., "Morning"
-//     range: String               // e.g., "07:00-09:00"
-//   }],
-// });
-
-// module.exports = mongoose.model("SlotTemplate", slotTemplateSchema);
-
-
-
-
 
 
 const mongoose = require("mongoose");
@@ -32,3 +17,22 @@ const slotTemplateSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("SlotTemplate", slotTemplateSchema);
+
+
+
+
+
+// const mongoose = require("mongoose");
+
+// const slotTemplateSchema = new mongoose.Schema({
+//   date: { type: String, required: true }, // "2025-07-01"
+//   slots: [{
+//     label: String,              // e.g., "Morning"
+//     range: String               // e.g., "07:00-09:00"
+//   }],
+// });
+
+// module.exports = mongoose.model("SlotTemplate", slotTemplateSchema);
+
+
+
