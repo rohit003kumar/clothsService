4e608f5ce9a3464a15cbfd266e2c328f2d10da8d
rohit003kumar <151962481+rohit003kumar@users.noreply.github.com>
2025-08-09 21:52:45 +0530

    Update predefine.route.js

--- Backend/routes/predefine.route.js
+++ Backend/routes/predefine.route.js
@@ -1,25 +1,6 @@
 
 
 
-// const express = require("express");
-// const router = express.Router();
-// const adminController = require("../controllers/predefine.controller");
-// const { isAuth, isAdmin } = require("../middleware/isAuth");
-
-// // Create or update slot templates
-// router.post("/slots", isAuth, isAdmin, adminController.createSlotTemplate);
-
-// // Get slots for a single date
-// // router.get("/slot-template", isAuth, isAdmin, adminController.getSlotTemplateByDate);
-
-// // Get all slot templates
-// router.get("/slot-templates", isAuth, isAdmin, adminController.getAllSlotTemplates);
-
-// // Delete slot template for a specific date
-// router.delete("/slot-template/:date", isAuth, isAdmin, adminController.deleteSlotTemplateByDate);
-
-// module.exports = router;
-
 
 
 
@@ -44,3 +25,26 @@ router.patch("/slot-template/:date/close", isAuth, isAdmin, adminController.togg
 router.patch("/slot-template/:date/slot/:slotLabel/close", isAuth, isAdmin, adminController.toggleSlotClosed);
 
 module.exports = router;
+
+
+
+
+
+// const express = require("express");
+// const router = express.Router();
+// const adminController = require("../controllers/predefine.controller");
+// const { isAuth, isAdmin } = require("../middleware/isAuth");
+
+// // Create or update slot templates
+// router.post("/slots", isAuth, isAdmin, adminController.createSlotTemplate);
+
+// // Get slots for a single date
+// // router.get("/slot-template", isAuth, isAdmin, adminController.getSlotTemplateByDate);
+
+// // Get all slot templates
+// router.get("/slot-templates", isAuth, isAdmin, adminController.getAllSlotTemplates);
+
+// // Delete slot template for a specific date
+// router.delete("/slot-template/:date", isAuth, isAdmin, adminController.deleteSlotTemplateByDate);
+
+// module.exports = router;
